86811e9de1431531e7d89c94bae1188c49797dbe
Silicon <axolium.prime@gmail.com>
2024-03-05 23:08:29 -0800

    -added cybex chunk item -added loot table for cybex ore
    Signed-off-by: Silicon <axolium.prime@gmail.com>

--- src/main/java/net/silicon/sector52/item/Sector52Items.java
+++ src/main/java/net/silicon/sector52/item/Sector52Items.java
@@ -12,8 +12,7 @@ import net.silicon.sector52.block.Sector52Blocks;
 public class Sector52Items {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Sector52.MOD_ID);
   
-  // public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-  //   .alwaysEat().nutrition(1).saturationMod(2f).build())));
+   public static final RegistryObject<Item> CYBEX_CHUNK_ITEM = ITEMS.register("cybex_chunk", () -> new Item(new Item.Properties()));
   
   // public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(Sector52Blocks.EXAMPLE_BLOCK.get(), new Item.Properties()));
   

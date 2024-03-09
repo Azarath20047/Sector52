f457e6a6fd2b599c90e897e6c4705d143c200664
bvanseg <bvanseg@gmail.com>
2024-03-08 21:51:46 -0500

    Fixed varda log using incorrect block type.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/net/silicon/sector52/block/Sector52Blocks.java
+++ src/main/java/net/silicon/sector52/block/Sector52Blocks.java
@@ -3,6 +3,7 @@ package net.silicon.sector52.block;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,6 @@ public class Sector52Blocks {
   public static final RegistryObject<Block> CYBEX_ORE_BLOCK = BLOCKS.register("cybex_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.COAL_ORE)));
   public static final RegistryObject<Block> TITANIUM_ORE_BLOCK = BLOCKS.register("titanium_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.COAL_ORE)));
   public static final RegistryObject<Block> VARDA_BIG_LEAVES_BLOCK = BLOCKS.register("varda_big_leaves", () -> new LeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)));
-  public static final RegistryObject<Block> VARDA_LOG_BLOCK = BLOCKS.register("varda_log", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)));
+  public static final RegistryObject<Block> VARDA_LOG_BLOCK = BLOCKS.register("varda_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)));
   public static final RegistryObject<Block> VARDA_SMALL_LEAVES_BLOCK = BLOCKS.register("varda_small_leaves", () -> new LeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)));
 }
